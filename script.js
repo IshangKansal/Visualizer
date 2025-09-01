73d7e28e301b137310f1fa7fef4c71a46777e2d0
Ishang Kansal <ishangkansal594@gmail.com>
2025-09-02 01:18:53 +0530

    added a run button

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let grid = Array.from({ length: 20 }, () => Array(20).fill(0));
 
-
 let CanPlace = true;
 
 let algorithm = "BFS";
 
-
-document.getElementById("menu").addEventListener("change", function() {
+document.getElementById("menu").addEventListener("change", function () {
     algorithm = this.value;
     console.log("myVar changed to:", algorithm);
 });
@@ -14,27 +12,33 @@ document.getElementById("menu").addEventListener("change", function() {
 const box = document.querySelector("#container");
 
 const marker = (i, j) => {
-    let curr = document.querySelector(`button[data-id = "${i}"][data-status = "${j}"]`);
-    if(grid[i][j] == 0){
-        grid[i][j] = 1;
-        curr.style.backgroundColor = "red";
-    }else{
-        grid[i][j] = 0;
-        curr.style.backgroundColor = "blue";
+    if (CanPlace) {
+        let curr = document.querySelector(
+            `button[data-id = "${i}"][data-status = "${j}"]`
+        );
+        if (grid[i][j] == 0) {
+            grid[i][j] = 1;
+            curr.style.backgroundColor = "red";
+        } else {
+            grid[i][j] = 0;
+            curr.style.backgroundColor = "blue";
+        }
+    } else {
+        alert("change to the placing mode");
     }
 };
 
 const MakeGrid = () => {
     let row = 0;
     let curr;
-    for(let i of grid){
+    for (let i of grid) {
         box.innerHTML += `<div id = "row${row}" style = "display: grid;"> </div>`;
         curr = document.querySelector(`#row${row}`);
-        for(let j in i){
+        for (let j in i) {
             curr.style.gridTemplateColumns += " 40px";
-            if(i[j] == 0){
+            if (i[j] == 0) {
                 curr.innerHTML += `<button data-id = "${row}" data-status = "${j}" onclick="marker(${row}, ${j})" style = "background-color: blue;"></button>`;
-            }else{
+            } else {
                 curr.innerHTML += `<button data-id = "${row}" data-status = "${j}" onclick="marker(${row}, ${j})" style = "background-color: red;"></button>`;
             }
         }
@@ -42,5 +46,4 @@ const MakeGrid = () => {
     }
 };
 
-(MakeGrid())();
-
+MakeGrid()();
