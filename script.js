f8be98480617924eb8ae4cc00b47093f88f71630
Ishang Kansal <ishangkansal594@gmail.com>
2025-09-02 00:47:51 +0530

    made the grid, displayed it, and made it interactive

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-let grid = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-]
+let grid = Array.from({ length: 20 }, () => Array(20).fill(0));
+
 
 let CanPlace = true;
+
+const box = document.querySelector("#container");
+
+const marker = (i, j) => {
+    let curr = document.querySelector(`button[data-id = "${i}"][data-status = "${j}"]`);
+    if(grid[i][j] == 0){
+        grid[i][j] = 1;
+        curr.style.backgroundColor = "red";
+    }else{
+        grid[i][j] = 0;
+        curr.style.backgroundColor = "blue";
+    }
+};
+
+const MakeGrid = () => {
+    let row = 0;
+    let curr;
+    for(let i of grid){
+        box.innerHTML += `<div id = "row${row}" style = "display: grid;"> </div>`;
+        curr = document.querySelector(`#row${row}`);
+        for(let j in i){
+            curr.style.gridTemplateColumns += " 40px";
+            if(i[j] == 0){
+                curr.innerHTML += `<button data-id = "${row}" data-status = "${j}" onclick="marker(${row}, ${j})" style = "background-color: blue;"></button>`;
+            }else{
+                curr.innerHTML += `<button data-id = "${row}" data-status = "${j}" onclick="marker(${row}, ${j})" style = "background-color: red;"></button>`;
+            }
+        }
+        row++;
+    }
+};
+
+(MakeGrid())();
 
